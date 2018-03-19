476849e1b8d951dbf030f9892520b9f7d30f4360
Shailesh Kumar Sharma <shaileshkumar0343@gmail.com>
2018-03-19 08:43:56 +0530

    Ui Update

--- app/src/main/java/io/traitsoft/tutor4u/LoginActivity.java
+++ app/src/main/java/io/traitsoft/tutor4u/LoginActivity.java
@@ -2,6 +2,7 @@ package io.traitsoft.tutor4u;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,6 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
     private Button phoneSignupButton;
-    final Context c = this;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,30 +28,8 @@ public class LoginActivity extends AppCompatActivity {
         phoneSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
-                View mView = layoutInflaterAndroid.inflate(R.layout.phone_varify_dailog, null);
-                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
-                alertDialogBuilderUserInput.setView(mView);
-
-                final EditText phoneInputDialogEditText = (EditText) mView.findViewById(R.id.phone_input_box);
-                final EditText otpInputDialogEditText = (EditText) mView.findViewById(R.id.otp_input_box);
-                alertDialogBuilderUserInput
-                        .setCancelable(false)
-                        .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogBox, int id) {
-                                // ToDo get user input here
-                            }
-                        })
-
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogBox, int id) {
-                                        dialogBox.cancel();
-                                    }
-                                });
-
-                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
-                alertDialogAndroid.show();
+                startActivity(new Intent(LoginActivity.this,OtpActivity.class));
+                overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
             }
         });
     }
---
+++ app/src/main/java/io/traitsoft/tutor4u/OtpActivity.java
@@ -0,0 +1,17 @@
+package io.traitsoft.tutor4u;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shail on 3/18/2018.
+ */
+
+public class OtpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
