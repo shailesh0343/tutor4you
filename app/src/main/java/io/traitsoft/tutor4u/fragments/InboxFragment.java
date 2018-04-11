961a21435ace75ed4042ea1f62ca07a5b4f70292
shailesh0343 <shaileshkumar0343@gmail.com>
2018-04-11 08:03:55 +0530

    Navigation Drawer Design

---
+++ app/src/main/java/io/traitsoft/tutor4u/fragments/InboxFragment.java
@@ -0,0 +1,35 @@
+package io.traitsoft.tutor4u.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.traitsoft.tutor4u.R;
+
+/**
+ * Created by shail on 4/1/2018.
+ */
+
+public class InboxFragment extends Fragment {
+
+    public InboxFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Inbox");
+    }
+}
---
+++ app/src/main/java/io/traitsoft/tutor4u/fragments/OngoingClassesFragment.java
@@ -0,0 +1,38 @@
+package io.traitsoft.tutor4u.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.traitsoft.tutor4u.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OngoingClassesFragment extends Fragment {
+
+
+    public OngoingClassesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ongoing_classes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Ongoing Classes");
+    }
+
+}
